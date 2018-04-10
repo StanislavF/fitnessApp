94c7851ca8faa71725dbcd5099db34ffcc682a8d
StanislavF <stanislavfotev95@gmail.com>
2018-04-10 08:44:23 +0300

    add trainer buttons in my-clients

--- src/app/content-holder/train-program/training-day/training-day.component.ts
+++ src/app/content-holder/train-program/training-day/training-day.component.ts
@@ -1,6 +1,7 @@
 import { Exercise } from './../../../shared/models/exercise.model';
 import { Component, OnInit, Input } from '@angular/core';
 import { TrainingDay } from '../../../shared/models/training-day.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-training-day',
@@ -10,11 +11,21 @@ import { TrainingDay } from '../../../shared/models/training-day.model';
 export class TrainingDayComponent implements OnInit {
 
   @Input() trainingDay: TrainingDay;
+  public isTrainer: boolean;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    let url = this.router.url;
+    let urlParts = url.split("/");
 
+    if(urlParts[2]==="my-clients"){
+      this.isTrainer=true;
+    } else {
+      this.isTrainer=false;
+    }
   }
 
 }
